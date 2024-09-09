2635a5e727ac05256c11767fde3553aa674bd6a3
Julius Neszvecsko <julius.neszvecsko@tum.de>
2024-09-09 17:46:23 +0200

    app modified

--- st_app.py
+++ st_app.py
@@ -9,32 +9,29 @@ st.set_page_config(layout="wide")
 st.title('Ergebnis-Manager')
 st.sidebar.title('Options')
 
-#@st.experimental_singleton(suppress_st_warning=True)
 def init_connection():
     # TODO: use details from secrets.toml file
-    uri = f"mongodb+srv://myc_start_boat:paY3SdvIYiryr84v@cluster0.3rpqm.mongodb.net/dsbl?retryWrites=true&w=majority"
+    uri = f"mongodb+srv://{st.secrets['mongo']['db_username']}:{st.secrets['mongo']['db_password']}@{st.secrets['mongo']['cluster_name']}.3rpqm.mongodb.net/dsbl?retryWrites=true&w=majority"
     return MongoClient(uri)
 
+pymongo = True
 
-client = init_connection()
+if pymongo:
+    client = init_connection()
 
+    try:
+        client.admin.command('ping')
+        print("[INFO] Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
 
-
-try:
-    print("[INFO] Pinging")
-    client.admin.command('ping')
-    print("[INFO] Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-db = client.dsbl
-collection_names = db.list_collection_names()
-collection_names.sort()
-EVENTS = len(collection_names)
+    db = client.dsbl
+    collection_names = db.list_collection_names()
+    collection_names.sort()
+    EVENTS = len(collection_names)
 
 # Initialize session state for data storage
 if 'data' not in st.session_state:
-    print("Initializing session state")
     st.session_state.data = {f'Event {event}': {} for event in range(1, EVENTS + 1)}
 
 # BOATS = st.sidebar.number_input('Number of Boats', value=BOATS, key='BOATS')
@@ -52,7 +49,7 @@ def initialize_pairing_result():
 
 
 def initialize_from_pymongo():
-    for event in range(len(collection_names)):
+    for event in range(0, len(collection_names)):
         pairing_list, results = utils.create_pairing_list(event)
         results = pd.DataFrame(db[collection_names[event]].find())
         results.drop(columns=['_id'], inplace=True)
@@ -61,8 +58,24 @@ def initialize_from_pymongo():
         st.session_state.data['Event {}'.format(event+1)]['results'] = results.reset_index(drop=True).to_dict('split')
 
 
+def write_to_pymongo():
+    for event in range(len(collection_names)):
+        df = pd.DataFrame(st.session_state.data[selected_event]['results']['data'],
+                              columns=st.session_state.data[selected_event]['results']['columns'],)
+        print(df.head())
+        collection = db[collection_names[event]]
+        collection.delete_many({})
+
+        data_dict = df.to_dict("records")
+        collection.insert_many(data_dict)
+
+
+
 if st.session_state.data['Event 1'] == {}:
-    initialize_from_pymongo()
+    if pymongo:
+        initialize_from_pymongo()
+    else:
+        initialize_pairing_result()
 
 
 selected_event = st.sidebar.selectbox('Select Event', options=[f'Event {i}' for i in range(1, EVENTS + 1)], index=0)
@@ -95,17 +108,27 @@ if st.session_state.data[selected_event]['pairing_list'] is not None:
         team = race_details[f'Boat{b}'].values[0]
 
         # TODO: initialize entry with value from results
-        results.append(st.sidebar.selectbox(f'Boat {b} - ' + team, options=[i for i in range(1, BOATS + 1)] + list(BUCHSTABEN.keys()), index=0,))
+        result_options = [i for i in range(1, BOATS + 1)] + list(BUCHSTABEN.keys())
+        flight = utils.get_flight(selected_race)
+        team_lookup = team.replace('(', ' (')
+        current_result = results_df.loc[results_df['Teams'] == team_lookup, f'Flight {flight}']
+        try:
+            current_result = current_result.astype(int)
+        except ValueError:
+            pass
+        try:
+            index = result_options.index(current_result.values[0])
+        except:
+            index = len(result_options) - 1
+        results.append(st.sidebar.selectbox(f'Boat {b} - ' + team, options=result_options, index=index,))
 
 
     if st.sidebar.button('Update Results'):
         for b, r in zip(range(1,utils.BOATS+1),results):
             if r:
                 team = race_details[f'Boat{b}'].values[0]
-                filtered_df = results_df[results_df['Teams'] == team]
-                row_index = filtered_df.index[0]
                 flight = utils.get_flight(selected_race)
-                results_df.loc[row_index, f'Flight {flight}'] = r
+                results_df.loc[results_df['Teams'] == team, f'Flight {flight}'] = r
 
         st.session_state.data[selected_event]['results'] = results_df.reset_index(drop=True).to_dict('split')
 
@@ -114,6 +137,8 @@ if st.session_state.data[selected_event]['pairing_list'] is not None:
     if st.sidebar.button('Update SCP'):
         filtered_df = results_df[results_df['Teams'] == team]
         row_index = filtered_df.index[0]
+        if scp == 0:
+            scp = np.nan
         results_df.loc[row_index, 'SCP'] = scp
         st.session_state.data[selected_event]['results'] = results_df.reset_index(drop=True).to_dict('split')
 
@@ -133,6 +158,10 @@ if st.session_state.data[selected_event]['results'] is not None:
     st.text('Results for {}'.format(selected_event))
     st.dataframe(results_df, height=750, use_container_width=True, hide_index=True,)
     st.sidebar.download_button(label="Download Results", data=results_df.to_csv(), file_name='results.csv', mime='text/csv')
+
+# TODO: save results to MongoDB
+write_to_pymongo()
+
 
 #Overall ranking
 st.text('Overall Results')
