255fab4e37133649a96ad9015a42844ab5707c9a
Julius Neszvecsko <julius.neszvecsko@tum.de>
2024-09-10 10:54:12 +0200

    modifications/styling

--- st_app.py
+++ st_app.py
@@ -8,6 +8,7 @@ from utils import BOATS, BUCHSTABEN, EVENTS
 st.set_page_config(layout="wide")
 st.title('Ergebnis-Manager')
 st.sidebar.title('Options')
+st.sidebar.divider()
 
 @st.cache_resource
 def init_connection():
@@ -29,9 +30,6 @@ EVENTS = len(collection_names)
 # Initialize session state for data storage
 if 'data' not in st.session_state:
     st.session_state.data = {f'Event {event}': {} for event in range(1, EVENTS + 1)}
-
-st.sidebar.divider()
-
 
 # Function to create pairing list and results (mimics the generate_pairing_list callback)
 def initialize_pairing_result():
@@ -65,16 +63,10 @@ results_df = pd.DataFrame(st.session_state.data[selected_event]['results']['data
                           columns=st.session_state.data[selected_event]['results']['columns'], )
 st.sidebar.download_button(label="Download Pairing List", data=pairing_list_df.to_csv(), file_name='pairing_list.csv', mime='text/csv', )
 
-if st.sidebar.button('Upload Results'):
-    uploaded_file = st.sidebar.file_uploader("Choose a file")
-    if uploaded_file is not None:
-        uploaded_results_df = pd.read_csv(uploaded_file)
-        st.text('Uploaded Results')
-        st.dataframe(uploaded_results_df, height=750, use_container_width=True, hide_index=True,)
-        st.session_state.data[selected_event]['results'] = uploaded_results_df.reset_index(drop=True).to_dict('split')
-
 # Display and download pairing list
 if st.session_state.data[selected_event]['pairing_list'] is not None:
+    st.sidebar.divider()
+    st.sidebar.text('Results')
     results_df = pd.DataFrame(st.session_state.data[selected_event]['results']['data'],
                               columns=st.session_state.data[selected_event]['results']['columns'], )
     results_df.reset_index(drop=True, inplace=True)
@@ -112,8 +104,11 @@ if st.session_state.data[selected_event]['pairing_list'] is not None:
         st.session_state.data[selected_event]['results'] = results_df.reset_index(drop=True).to_dict('split')
         write_to_pymongo(results_df, int(selected_event.split()[1]) - 1)
 
+    st.sidebar.divider()
+    st.sidebar.text('Scoring Penalties')
     team = st.sidebar.selectbox('Select Team', options=results_df['Teams'].sort_values(), index=0)
     scp = st.sidebar.number_input('Enter SCP', value=0)
+
     if st.sidebar.button('Update SCP'):
         filtered_df = results_df[results_df['Teams'] == team]
         row_index = filtered_df.index[0]
@@ -142,6 +137,7 @@ if st.session_state.data[selected_event]['results'] is not None:
         pass
     st.text('Results for {}'.format(selected_event))
     st.dataframe(results_df, height=750, use_container_width=True, hide_index=True,)
+    st.sidebar.divider()
     st.sidebar.download_button(label="Download Results", data=results_df.to_csv(), file_name='results.csv', mime='text/csv')
 
 
@@ -171,6 +167,7 @@ try:
     overall_results.drop(columns=['Rank'], inplace=True)
 except KeyError:
     pass
+
 overall_results.insert(0, 'Rank', range(1, overall_results.shape[0] + 1))
 st.dataframe(overall_results, height=750, use_container_width=True, hide_index=True,)
 
